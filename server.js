889ec22f721fb2043064363454f79854fdf741ff
frixxus <frixxus@gmail.com>
2023-05-09 22:29:52 -0700

    fix: get res send wrong object, no output on app

--- server.js
+++ server.js
@@ -8,8 +8,20 @@ const PORT = process.env.PORT || 4000
 
 app.use(express.static('public'))
 
-app.listen(PORT, () => {})
+app.listen(PORT, () => {
+	console.log('Running')
+})
 
 app.get('/api/quotes/random', (req, res, next) => {
-	res.send(getRandomElement(quotes))
+	const randomQuote = getRandomElement(quotes)
+	res.send({ quote: randomQuote })
+})
+
+app.get('/api/quotes', (req, res, next) => {
+	// const foundPerson = route.query.person
+	// if (foundPerson) {
+	// } else {
+	// 	res.send(quotes)
+	// }
+	res.send({ quotes: quotes })
 })
